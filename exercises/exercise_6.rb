6a090496ef97da6e0413b551e8a0fcb695a00b35
Kiup Kim <kukim96@gmail.com>
2021-05-24 17:17:24 -0600

    finished exercise 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "David", last_name: "Kim", hourly_rate: 100)
+@store1.employees.create(first_name: "Daniel", last_name: "Kim", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Serah", last_name: "Lee", hourly_rate: 200)
+@store2.employees.create(first_name: "Jason", last_name: "Kim", hourly_rate: 90)
+@store2.employees.create(first_name: "Kevin", last_name: "Lee", hourly_rate: 70)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
 
+  validates :first_name, :last_name, presence: true
+
+  validates :hourly_rate, numericality: {only_integer: true, greater_than: 59, less_than: 201}
+
+  validates :store_id, presence: true
 end
